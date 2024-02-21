0f66a0589d186c89d92c5450ae7605dd102ed16c
Eureka <teklemariameureka@gmail.com>
2024-02-21 10:04:00 +0300

    Add js-prac

---
+++ js-prac/one.js
@@ -0,0 +1,8 @@
+// Write a function called add7 that takes one number and returns that number + 7.
+
+
+function add7(num){
+    return num +7
+}
+
+console.log(add7(7))
---
+++ js-prac/two.js
@@ -0,0 +1,7 @@
+// Write a function called multiply that takes 2 numbers and returns their product.\\
+
+function multiply(num1,num2){
+    return num1 * num2
+}
+
+console.log(multiply(12,2))
